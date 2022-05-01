76e474a1dabdb7cc70de0b28da10045de37e1af1
opusaha <sahaapo@gmail.com>
2022-05-01 09:50:08 +0600

    structure design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-
+import Header from './components/header/Header';
+import Shop from './components/shop/Shop'
 function App() {
   return (
     <div>
       <Header/>
+      <Shop/>
     </div>
   );
 }
